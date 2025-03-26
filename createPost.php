ef885c5cebd9b2b865feeb7f13c3373c8fa10a57
LucaMawyin <lucamawyin@gmail.com>
2025-03-26 12:43:16 -0400

    started create post page

---
+++ createPost.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+        <title>Create Post</title>
+        <link rel="stylesheet" href="css/global.css">
+    </head>
+
+    <body>
+        
+    </body>
+</html>
