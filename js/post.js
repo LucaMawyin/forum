480041a8af2badc76b9e1b333290055aed4a94b0
Neelan <neelan2317@gmail.com>
2025-03-28 17:48:10 -0400

    Add UI for reply form

--- js/post.js
+++ js/post.js
@@ -1,13 +1,17 @@
 window.addEventListener("load", () => {
-    
+    let cancelForm = document.querySelector(".btn-cancel");
+    let addReplyBtn = document.getElementById("addreply");
+    let replyform = document.getElementById("replyeditor");
+    console.log(cancelForm);
     $("#reply-tab").on("click", function (){
         $("#replies").toggle();
         if ($("#replies").is(":visible")){
             $("#reply-tab span").html("&#x25B2;");
-            
+
         }
         else {
             $("#reply-tab span").html("&#x25BC;");
+            $("#replyeditor")[0].style.display = "none";
         }
         
     });
@@ -16,9 +20,10 @@ window.addEventListener("load", () => {
         e.stopPropagation();
         $("#replies").show();
         $("#reply-tab span").html("&#x25B2;");
-
-
+        $("#replyeditor")[0].style.display = "flex";
     });
-    
+    cancelForm.addEventListener("click", () => {
+        replyform.style.display = "none";
 
+    })
 });
